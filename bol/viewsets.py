b297edf3ee283b4badeaaf619c55edf8658282ac
Ajay Katwe <ajay.katwe@gmail.com>
2020-05-01 22:31:50 +0530

    :art: Make non client views read-only

--- bol/viewsets.py
+++ bol/viewsets.py
@@ -1,4 +1,4 @@
-from rest_framework.viewsets import  ModelViewSet
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
 from bol.models import Client, ShipmentItem , Transport , Customer ,Shipment
 from bol.serializers import ClientSerializer, TransportSerializer, CustomerSerializer, ShipmentItemSerializer, ShipmentSerializer
@@ -9,21 +9,21 @@ class ClientViewSet(ModelViewSet):
     serializer_class = ClientSerializer
 
 
-class TransportViewSet(ModelViewSet):
+class TransportViewSet(ReadOnlyModelViewSet):
     queryset = Transport.objects.filter()
     serializer_class = TransportSerializer
 
 
-class CustomerViewSet(ModelViewSet):
+class CustomerViewSet(ReadOnlyModelViewSet):
     queryset = Customer.objects.filter()
     serializer_class = CustomerSerializer
 
 
-class ShipmentItemViewSet(ModelViewSet):
+class ShipmentItemViewSet(ReadOnlyModelViewSet):
     queryset = ShipmentItem.objects.filter()
     serializer_class = ShipmentItemSerializer
 
 
-class ShipmentViewset(ModelViewSet):
+class ShipmentViewset(ReadOnlyModelViewSet):
     queryset = Shipment.objects.filter()
     serializer_class = ShipmentSerializer
