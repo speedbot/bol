e482df3646ffbfa746017a9b1fe064d42ec880c0
Ajay Katwe <ajay.katwe@gmail.com>
2020-05-02 15:48:09 +0530

    :art: Update API handler

--- bol/handler.py
+++ bol/handler.py
@@ -1,30 +1,32 @@
 import json
 import requests
+from .constants import BOL_LOGIN_URL, BOL_SHIPMENT_URL
 
 
 class APIHandler:
     def __init__(self, client):
         self.client = client
-        self.auth_token_url = 'https://login.bol.com/token?grant_type=client_credentials'
-        self.shipment_list_url = 'https://api.bol.com/retailer/shipments/'
 
     def get_access_token(self):
+        if not self.client.is_expired():
+            return self.client.auth_token
         kwargs = {
             'client_id': self.client.client_id,
             'client_secret': self.client.client_secret,
             'grant_type': 'client_credentails',
         }
-        response = requests.post(self.auth_token_url, kwargs)
+        response = requests.post(BOL_LOGIN_URL, kwargs)
         if response.status_code == 200:
-            self.client.update_token_expiry()
-            return json.loads(response.content.decode())['access_token']
+            auth_token = json.loads(response.content.decode())['access_token']
+            self.client.update_auth_token(auth_token)
+            return auth_token
         else:
             return None
 
     def get_all_shipments(self):
         header = 'Bearer {}'.format(self.get_access_token())
         response = requests.get(
-            self.shipment_list_url,
+            BOL_SHIPMENT_URL,
             headers={'Authorization': header, 'Accept': 'application/vnd.retailer.v3+json'},
         )
         return json.loads(response.content.decode())
