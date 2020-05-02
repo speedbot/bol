eaffb22e05bc25b84f8d9beb17698faacd7775f0
Ajay Katwe <ajay.katwe@gmail.com>
2020-05-02 15:47:14 +0530

    :art: Add auth_token attr on Client model

--- bol/models.py
+++ bol/models.py
@@ -17,14 +17,16 @@ class Client(TimeStampMixin, models.Model):
     client_id = models.CharField(max_length=255, null=False)
     client_secret = models.CharField(max_length=255, null=False)
     expiry_date = models.DateTimeField(default=None)
+    auth_token = models.CharField(max_length=255, default='')
 
     def is_expired(self):
-        if self.expiry_date and timezone.now() < self.expiry_date:
+        if self.expiry_date and timezone.now() > self.expiry_date:
             return True
         else:
             return False
 
-    def update_token_expiry(self):
+    def update_auth_token(self, auth_token):
+        self.auth_token = auth_token
         self.expiry_date = self.expiry_date + timedelta(minutes=5)
         self.save()
 
