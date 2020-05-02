9c726130a579405730287df858df54ec7fb73d5f
Ajay Katwe <ajay.katwe@gmail.com>
2020-05-02 15:54:17 +0530

    :art: DRY header generation logic

--- bol/handler.py
+++ bol/handler.py
@@ -23,10 +23,20 @@ class APIHandler:
         else:
             return None
 
-    def get_all_shipments(self):
+    def get_headers(self):
         header = 'Bearer {}'.format(self.get_access_token())
+        return {'Authorization': header, 'Accept': 'application/vnd.retailer.v3+json'}
+
+    def get_all_shipments(self):
         response = requests.get(
             BOL_SHIPMENT_URL,
-            headers={'Authorization': header, 'Accept': 'application/vnd.retailer.v3+json'},
+            headers=self.get_headers(),
+        )
+        return json.loads(response.content.decode())
+
+    def get_shipment(self, shipmentId):
+        response = requests.get(
+            '{}{}/'.format(BOL_SHIPMENT_URL, shipmentId),
+            headers=self.get_headers(),
         )
         return json.loads(response.content.decode())
