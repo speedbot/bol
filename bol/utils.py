c0844f354c0062bdb7221fcec5b41e0f1830b1df
Ajay Katwe <ajay.katwe@gmail.com>
2020-05-02 19:41:26 +0530

    :art: Add util method to clean db for testing purpose

--- bol/utils.py
+++ bol/utils.py
@@ -1,5 +1,5 @@
 import logging
-
+from bol.models import Shipment, ShipmentItem,Transport,Customer
 
 def get_task_logger():
     return logging.getLogger('task_exceptions')
@@ -7,3 +7,10 @@ def get_task_logger():
 
 def tuple_instance(instance):
     return (instance._meta.app_label, instance._meta.model_name, instance.id)
+
+
+def clean_db():
+    models = [ShipmentItem, Transport, Customer, Shipment]
+    for model in models:
+        for item in model.objects.filter():
+            item.delete()
