adc8fa8a87f4d2a5c971fd4327be0cbd36473354
Ajay Katwe <ajay.katwe@gmail.com>
2020-05-03 19:28:15 +0530

    :fire: Remove unused methods

--- bol/utils.py
+++ bol/utils.py
@@ -7,10 +7,6 @@ def get_task_logger():
     return logging.getLogger('task_exceptions')
 
 
-def tuple_instance(instance):
-    return (instance._meta.app_label, instance._meta.model_name, instance.id)
-
-
 def clean_db():
     models = [ShipmentItem, Transport, Customer, Shipment]
     for model in models:
